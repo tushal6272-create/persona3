8cb9dc47125fff66cf637e71ad90ffe7bda8a015
tushal6272-create <tushal6272@gmail.com>
2026-05-18 10:09:38 +0530

    Fix Netlify static build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,6 @@
 import { dev } from "$app/environment";
 import { injectAnalytics } from "@vercel/analytics/sveltekit";
 
+export const prerender = true;
+
 injectAnalytics({ mode: dev ? "development" : "production" });
